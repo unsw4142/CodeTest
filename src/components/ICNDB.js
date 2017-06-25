346d5fa5d8d10e8ef855fc9bf44688e59724c2cd
Rahul Singh <rahulsinghunsw@gmail.com>
2017-06-25 15:47:36 +1000

    Fixed, still Network Error

--- src/components/ICNDB.js
+++ src/components/ICNDB.js
@@ -1,67 +1,48 @@
 import React, {Component} from 'react';
-import {Image, View, Text, ListView, StyleSheet} from 'react-native';
-//var math = require('mathjs');
-// 3 digits n
-var n = 5;
-var JOKES_URL = 'http://www.icndb.com/api/jokes/random';
+import {Image, View, Text, ListView, StyleSheet, ActivityIndicator} from 'react-native';
+
 
 class ICNDB extends Component {
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
     this.state = {
-      requestFailed: false,
-      jokes: '',
+      isLoading: true,
+      jokes: [],
     };
   };
-
-  getJokes(rjokes){
-    this.setState(jokes: rjoke);
-  }
   // To Make network request, as per react
   // Automatically called by React
   componentDidMount() {
       // call the fetch function
       // Input - API url
-      fetch(JOKES_URL)
-        //To check if the response recieved is OKAY
-        .then(res =>{
-          if(!res.ok){
-            // Error by this
-            throw Error("Network req failed")
-          }
-        })
+      return fetch('http://api.icndb.com/jokes/random.json')
         // then this
-        .then(res => res.json())
-        .then(res => {
-            this.setState({
-            data: res,
-            jokes: res.value.joke,
-            });
-        },
-        // Not applying catch here
-        // One of these will work
-        () =>{
+        .then((res) => res.json())
+        .then(resJSON => {
+          let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
           this.setState({
-            requestFailed: true
-          })
+            isLoading: false,
+            dataSource: ds.cloneWithRows(resJSON.value),
+            jobs: resJSON.value.jokes
+          }, function() {
+            // do something with new state
+          });
+        })
+        .catch((error) => {
+          console.error(error);
         });
   }
-  renderJokes(inp){
-    return(
-      <View>
-        <Text> {inp.value.joke} </Text>
-      </View>
-  );
-  }
   render() {
-    //If it connects to the url
-    //if(this.state.requestFailed) return <Text> Failed Network </Text>
+
     // First time it renders, no data defined
-    //if(!this.state.data) return <Text> Loading </Text>
+    if(this.state.isLoading) return <Text> Loading </Text>;
+
     return (
-      <View>
-        <ListView dataSource= {this.state.data}
-                  renderRow = {this.renderJokes}> </ListView>
+      <View >
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={(Data) => <Text>{Data.joke}</Text>}
+        />
       </View>
     );
   }
