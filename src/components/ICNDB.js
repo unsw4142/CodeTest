1d2bcf20a9d4defd72f11e46e26e84051aac9831
Rahul Singh <rahulsinghunsw@gmail.com>
2017-06-25 16:04:55 +1000

    Blank Page??

--- src/components/ICNDB.js
+++ src/components/ICNDB.js
@@ -12,10 +12,10 @@ class ICNDB extends Component {
   };
   // To Make network request, as per react
   // Automatically called by React
-  componentDidMount() {
+  componentWillMount() {
       // call the fetch function
-      // Input - API url
-      return fetch('http://api.icndb.com/jokes/random.json')
+      // Input - API url- https
+      return fetch('https://api.icndb.com/jokes/random.json')
         // then this
         .then((res) => res.json())
         .then(resJSON => {
@@ -24,8 +24,6 @@ class ICNDB extends Component {
             isLoading: false,
             dataSource: ds.cloneWithRows(resJSON.value),
             jobs: resJSON.value.jokes
-          }, function() {
-            // do something with new state
           });
         })
         .catch((error) => {
@@ -35,7 +33,7 @@ class ICNDB extends Component {
   render() {
 
     // First time it renders, no data defined
-    if(this.state.isLoading) return <Text> Loading </Text>;
+    if(this.state.isLoading) return <Text style = {{padding: 20}}> Loading.... </Text>;
 
     return (
       <View >
@@ -43,6 +41,7 @@ class ICNDB extends Component {
           dataSource={this.state.dataSource}
           renderRow={(Data) => <Text>{Data.joke}</Text>}
         />
+      <Text> {this.state.jokes}</Text>
       </View>
     );
   }
